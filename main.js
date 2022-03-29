c8b9586fcedd2977e1bcf1449a7a2d6b9cf226aa
andresjair <sotorodriguez4140@gmail.com>
2022-03-28 21:55:31 -0500

    Maquetacion terminada con servidor funconando

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // https://andyelpoeta.000webhostapp.com/Maquetacion_php/api.php
 addEventListener("DOMContentLoaded", async(e)=>{
-    let peticion = await fetch("api.php");
+    let peticion = await fetch("https://andyelpoeta.000webhostapp.com/Maquetacion_php/api.php");
     let json = await peticion.json();
     console.log(json);
     document.querySelector(".logo").insertAdjacentHTML("beforeend", json.imagen);
